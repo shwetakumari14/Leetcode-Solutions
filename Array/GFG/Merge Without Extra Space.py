9cc29fdcfdc06367e9e89cb81b6c265ce23aa158
Shweta Kumari <shweta.kumari@go-mmt.com>
2022-07-11 21:55:55 +0530

    Merge without extra space

--- Array/GFG/Merge Without Extra Space.py
+++ Array/GFG/Merge Without Extra Space.py
@@ -1,19 +1,20 @@
 class Solution:
-    def mergeArrays(self, arr1, n, arr2, m):
-        for i in range(m):
-            arr1.append(0)
+    def mergeArrays2(self, arr1, n, arr2, m):
+        i, j = n-1, 0
 
-        while m:
-            if n and arr1[n - 1] >= arr2[m-1]:
-                arr1[m+n-1] = arr1[n-1]
-                n -= 1
-            else:
-                arr1[m+n-1] = arr2[m-1]
-                m -= 1
+        while i >=0 and j < m:
+            if arr1[i] > arr2[j]:
+                arr1[i], arr2[j] = arr2[j], arr1[i]
+
+            i -= 1
+            j += 1
         
-        return arr1
+        arr1.sort()
+        arr2.sort()
+        
+        return arr1, arr2
 
 obj = Solution()
 arr1, arr2, n, m = [1, 3, 5, 7], [0, 2, 6, 8, 9], 4, 5
-ans = obj.mergeArrays(arr1, n, arr2, m)
-print(ans)
+ans1, ans2 = obj.mergeArrays2(arr1, n, arr2, m)
+print(ans1, ans2)
