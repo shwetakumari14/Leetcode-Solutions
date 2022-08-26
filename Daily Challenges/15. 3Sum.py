1bd040986cd917802cc59fa8f3116bbab7138346
Shweta Kumari <shwetakumari@Shwetas-MBP.lan>
2022-08-25 20:21:39 -0700

    Three sum

---
+++ Daily Challenges/15. 3Sum.py
@@ -0,0 +1,24 @@
+class Solution:
+    def threeSum(self, nums):
+        nums.sort()
+        n = len(nums)
+        ans = []
+        for i in range(n-1):
+            l, r, x = i+1, n-1, nums[i]
+            
+            while l < r:
+                if x + nums[l] + nums[r] == 0:
+                    ans.append([x, nums[l], nums[r]])
+                    l += 1
+                    r -= 1
+                elif x + nums[l] + nums[r] < 0:
+                    l += 1
+                else:
+                    r -= 1
+                    
+        return [list(x) for x in set(tuple(x) for x in ans)]
+
+obj = Solution()
+arr = [-1,0,1,2,-1,-4]
+ans = obj.threeSum(arr)
+print(ans)
