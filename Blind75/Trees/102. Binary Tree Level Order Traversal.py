291f7c9ba4a9e4e75d6f1b782162aaaeaaa05a55
Shweta Kumari <shwetakumari@Shwetas-MacBook-Pro.local>
2022-09-08 20:47:44 -0700

    Binary tree level order traversal

---
+++ Blind75/Trees/102. Binary Tree Level Order Traversal.py
@@ -0,0 +1,39 @@
+from collections import deque
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def levelOrder(self, root):
+        if not root:
+            return []
+        
+        queue, ans = deque([root]), []
+
+        while queue:
+            currLevel, size = [], len(queue)
+            for i in range(size):
+                node = queue.popleft()
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+                currLevel.append(node.val)
+
+            ans.append(currLevel)
+        
+        return ans
+
+       
+
+obj = Solution()
+root = TreeNode(3)
+root.left = TreeNode(9)
+root.right = TreeNode(20)
+root.right.left = TreeNode(15)
+root.right.right = TreeNode(7)
+ans = obj.levelOrder(root)
+print(ans)
