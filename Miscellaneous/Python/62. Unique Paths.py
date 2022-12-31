879e939e08d62a2c1503853f532f4276aa10e440
Shweta Kumari <shwetakumari@Shwetas-MBP.lan>
2022-12-30 16:58:18 -0800

    Unique paths one

--- Miscellaneous/Python/62. Unique Paths.py
+++ Miscellaneous/Python/62. Unique Paths.py
@@ -1,18 +1,17 @@
 class Solution:
-    def uniquePaths(self, p, q):
-        m, n = int(p), int(q)
-        dp = [1 for i in range(n)]
+    def uniquePaths(self, m, n):
+        dpArray = [[1]*n for i in range(m)]
 
-        for i in range(m-1):
+        for i in range(1, m):
             for j in range(1, n):
-                dp[j] += dp[j-1]
+                dpArray[i][j] = dpArray[i-1][j] + dpArray[i][j-1]
         
-        return dp[n-1]
+        return dpArray[-1][-1]
 
         
 
 obj = Solution()
-n, m = "3", "3"
+n, m = 3, 7
 ans = obj.uniquePaths(m, n)
 print(ans)
                     
