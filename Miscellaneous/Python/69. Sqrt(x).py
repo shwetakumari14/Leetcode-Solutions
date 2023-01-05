568264cbf178a0fde8ce1b9ec580626c3b4220bb
Shweta Kumari <shwetakumari@Shwetas-MBP.lan>
2023-01-04 22:50:12 -0800

    Square root of a number

--- Miscellaneous/Python/69. Sqrt(x).py
+++ Miscellaneous/Python/69. Sqrt(x).py
@@ -2,21 +2,22 @@ class Solution:
     def sqrt(self, x):
         if x == 0:
             return 0
+        
+        left, right = 1, x - 1
 
-        left, right = 1, x
-
-        while left < right -    1:
+        while left < right - 1:
             mid = left + (right - left)//2
             if mid <= x//mid:
                 left = mid
             else:
                 right = mid - 1
         
-        if right <= x//right:
+        if right <= x // right:
             return right
+        
         return left
 
 
 obj = Solution()
-ans = obj.sqrt(17)
+ans = obj.sqrt(27)
 print(ans)
